9dcd149f1c3ea65ccc80b9341a4bf88eb4e7bc94
shinoburc <dandydot@gmail.com>
2016-03-29 01:49:06 +0900

    fix

--- PrimeFactorization.pl
+++ PrimeFactorization.pl
@@ -5,6 +5,7 @@
 # prime_factorization(28) -> 1, 2, 2, 7
 
 use strict;
+use POSIX qw(ceil);
 
 foreach my $number (1..100){
   print $number . ":1";
@@ -15,7 +16,7 @@ foreach my $number (1..100){
 sub print_prime_factorization($){
   my $number = shift;
   
-  foreach my $divisor(2..($number)){
+  foreach my $divisor(2..(ceil(sqrt($number)))){
     if($number % $divisor == 0){
       print "," . $divisor;
       &print_prime_factorization($number / $divisor);
